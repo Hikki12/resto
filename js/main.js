e8169c7d418ad48f530e861d7a1f9829d8e550b5
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-08-06 11:36:41 -0500

    added home section

--- js/main.js
+++ js/main.js
@@ -16,11 +16,31 @@ window.addEventListener('scroll', ()=>{
 });
 
 searchIcon.addEventListener('click', () => {
-    console.log('search toggle')
     searchForm.classList.toggle('active');
 });
 
 closeIcon.addEventListener('click', () => {
-    console.log('holu')
     searchForm.classList.remove('active');
-})
+});
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
